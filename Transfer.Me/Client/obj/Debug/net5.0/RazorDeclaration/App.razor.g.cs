8fc614740042d68eaace4d1214de72f12d1ef044
Davis Henckel <51898571+DavisHenckel@users.noreply.github.com>
2021-11-13 07:13:23 -0800

    update gitignore

--- Transfer.Me/Client/obj/Debug/net5.0/RazorDeclaration/App.razor.g.cs
+++ Transfer.Me/Client/obj/Debug/net5.0/RazorDeclaration/App.razor.g.cs
@@ -13,70 +13,70 @@ namespace Transfer.Me.Client
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\alexg\source\repos\transfer.me\Transfer.Me\Client\_Imports.razor"
+#line 1 "D:\Users\Davis\Computer Science\OSU\Senior Project\transfer.me\Transfer.Me\Client\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\alexg\source\repos\transfer.me\Transfer.Me\Client\_Imports.razor"
+#line 2 "D:\Users\Davis\Computer Science\OSU\Senior Project\transfer.me\Transfer.Me\Client\_Imports.razor"
 using System.Net.Http.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\alexg\source\repos\transfer.me\Transfer.Me\Client\_Imports.razor"
+#line 3 "D:\Users\Davis\Computer Science\OSU\Senior Project\transfer.me\Transfer.Me\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\alexg\source\repos\transfer.me\Transfer.Me\Client\_Imports.razor"
+#line 4 "D:\Users\Davis\Computer Science\OSU\Senior Project\transfer.me\Transfer.Me\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\alexg\source\repos\transfer.me\Transfer.Me\Client\_Imports.razor"
+#line 5 "D:\Users\Davis\Computer Science\OSU\Senior Project\transfer.me\Transfer.Me\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\alexg\source\repos\transfer.me\Transfer.Me\Client\_Imports.razor"
+#line 6 "D:\Users\Davis\Computer Science\OSU\Senior Project\transfer.me\Transfer.Me\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\alexg\source\repos\transfer.me\Transfer.Me\Client\_Imports.razor"
+#line 7 "D:\Users\Davis\Computer Science\OSU\Senior Project\transfer.me\Transfer.Me\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\alexg\source\repos\transfer.me\Transfer.Me\Client\_Imports.razor"
+#line 8 "D:\Users\Davis\Computer Science\OSU\Senior Project\transfer.me\Transfer.Me\Client\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\alexg\source\repos\transfer.me\Transfer.Me\Client\_Imports.razor"
+#line 9 "D:\Users\Davis\Computer Science\OSU\Senior Project\transfer.me\Transfer.Me\Client\_Imports.razor"
 using Transfer.Me.Client;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\alexg\source\repos\transfer.me\Transfer.Me\Client\_Imports.razor"
+#line 10 "D:\Users\Davis\Computer Science\OSU\Senior Project\transfer.me\Transfer.Me\Client\_Imports.razor"
 using Transfer.Me.Client.Shared;
 
 #line default
