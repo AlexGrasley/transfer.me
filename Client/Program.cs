94886a81149ea3373176a0d59ed3d84686912c51
Davis Henckel <51898571+DavisHenckel@users.noreply.github.com>
2021-12-29 09:27:25 -0800

    Completed updating project to .NET6

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,10 +1,12 @@
 using Client;
+using MudBlazor.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddMudServices();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
