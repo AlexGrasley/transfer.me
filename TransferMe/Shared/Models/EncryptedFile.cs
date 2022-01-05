5430f4eb0c1c38f3e037c4b254d3f923a0896fb7
AlexGrasley <alexgrasley@gmail.com>
2022-01-05 06:31:16 -0800

    Add project files.

---
+++ TransferMe/Shared/Models/EncryptedFile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Transfer.Me.Shared.Models
+{
+    public class EncryptedFile
+    {
+        public long FileID { get; set; }
+	    public long FileSize { get; set; }
+        public string Descript { get; set; }
+        public byte[] FileData { get; set; }
+        public long UserID { get; set; }
+    }
+}
