bb9de7b7aef0089eecec12f7ef43d0a25e88d1d3
AlexGrasley <alexgrasley@gmail.com>
2022-02-02 16:03:06 -0800

    Added EncFile model to Shared, created CosmosDB Interface and implementation of interface for CRUD operations. Updated API to take in an EncFile object from the POST body and loading it into the database.

---
+++ Server/Services/CosmosDbService.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Azure.Cosmos;
+using Microsoft.Azure.Cosmos.Fluent;
+using Microsoft.Extensions.Configuration;
+using Shared.Models;
+
+namespace Server
+{
+
+
+    public class CosmosDbService : ICosmosDbService
+    {
+        private Container _container;
+
+        public CosmosDbService(
+            CosmosClient dbClient,
+            string databaseName,
+            string containerName)
+        {
+            this._container = dbClient.GetContainer(databaseName, containerName);
+        }
+
+        public async Task AddItemAsync(EncFile file)
+        {
+            await this._container.CreateItemAsync<EncFile>(file, new PartitionKey(file.FileID));
+        }
+
+        public async Task DeleteItemAsync(string id)
+        {
+            await this._container.DeleteItemAsync<EncFile>(id, new PartitionKey(id));
+        }
+
+        public async Task<EncFile> GetItemAsync(string id)
+        {
+            try
+            {
+                ItemResponse<EncFile> response = await this._container.ReadItemAsync<EncFile>(id, new PartitionKey(id));
+                return response.Resource;
+            }
+            catch (CosmosException ex) when (ex.StatusCode == System.Net.HttpStatusCode.NotFound)
+            {
+                return null;
+            }
+
+        }
+
+        public async Task<IEnumerable<EncFile>> GetItemsAsync(string queryString)
+        {
+            var query = this._container.GetItemQueryIterator<EncFile>(new QueryDefinition(queryString));
+            List<EncFile> results = new List<EncFile>();
+            while (query.HasMoreResults)
+            {
+                var response = await query.ReadNextAsync();
+
+                results.AddRange(response.ToList());
+            }
+
+            return results;
+        }
+
+        public async Task UpdateItemAsync(string id, EncFile file)
+        {
+            await this._container.UpsertItemAsync<EncFile>(file, new PartitionKey(id));
+        }
+    }
+}
