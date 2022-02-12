6efc665e46ab424d89b692f401dbfd2ddb7f1df4
Davis Henckel <51898571+DavisHenckel@users.noreply.github.com>
2022-02-12 10:53:37 -0800

    Initial commit of UserAuth. Constrcutor and class built

---
+++ Server/API/UserAuth.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Cosmos;
+using Shared.Models;
+
+namespace Server
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserAuth : ControllerBase
+    {
+        public CosmosClient _client;
+        private readonly ILogger _logger;
+
+        //Constructor
+        public UserAuth(ILogger<UserAuth> logger, CosmosClient client)
+        {
+            _logger = logger;
+            _client = client;
+        }
+    }
+}
