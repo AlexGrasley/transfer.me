638e76f34983a88aff72c54d5ed93b2d47b3712f
Davis Henckel <51898571+DavisHenckel@users.noreply.github.com>
2022-02-22 20:39:21 -0800

    Convert password to byte array, hash it in string form, store in db as byte array. Using BCrypt

--- Client/Code/TransferMeUser.cs
+++ Client/Code/TransferMeUser.cs
@@ -1,31 +1,41 @@
 ﻿using Newtonsoft.Json;
+using Org.BouncyCastle.Security;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace Client.Code
 {
     public class TransferMeUser
     {
         [JsonProperty(PropertyName = "id")]
-        public string? ID { get; set; }
+        public string ID { get; set; }
 
         [JsonProperty(PropertyName = "UserID")]
-        public string? UserID { get; set; }
+        public string UserID { get; set; }
 
         [JsonProperty(PropertyName = "Username")]
-        public string? Username { get; set; }
+        public string Username { get; set; }
 
         [JsonProperty(PropertyName = "Password")]
-        public string? Password { get; set; }
+        public byte[] Password { get; set; }
 
         [JsonProperty(PropertyName = "Email Address")]
-        public string? EmailAddress { get; set; }
+        public string EmailAddress { get; set; }
 
-        public TransferMeUser(string Username, string EmailAddress, string Password)
+        public TransferMeUser(string Username, string EmailAddress, byte[] Password)
         {
             this.Username = Username;
-            this.Password = Password.GetHashCode().ToString();
+            this.Password = Password;
+            HashPassword();
             this.EmailAddress = EmailAddress;
             ID = Guid.NewGuid().ToString();
             UserID = ID;
+        }
+
+        public void HashPassword()
+        {
+            string HashedBytes = BCrypt.Net.BCrypt.HashPassword(Encoding.UTF8.GetString(Password, 0, Password.Length));
+            Password = Encoding.UTF8.GetBytes(HashedBytes);
         }
     }
 }
