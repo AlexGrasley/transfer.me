24024ed4b6abc05fea66d55708c334291f254d23
Davis Henckel <51898571+DavisHenckel@users.noreply.github.com>
2022-02-26 11:23:22 -0800

    Remove Client.Code and change it to client.Models for the models used in Client.

---
+++ Client/Models/SignInRequest.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.Models
+{
+    public class SignInRequest
+    {
+        [JsonProperty(PropertyName = "Username")]
+        public string Username { get; set; }
+
+        [JsonProperty(PropertyName = "Password")]
+        public string Password { get; set; }
+
+        public SignInRequest(string Username, string Password)
+        {
+            this.Username = Username;
+            this.Password = Password.GetHashCode().ToString();
+        }
+    }
+}
