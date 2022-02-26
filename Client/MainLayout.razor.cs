56c1d6226598b84e5ac676e68a4a40b187f45202
Davis Henckel <51898571+DavisHenckel@users.noreply.github.com>
2022-02-26 10:56:34 -0800

    Move Drawer to code behind. Update all namespaces

---
+++ Client/MainLayout.razor.cs
@@ -0,0 +1,24 @@
+﻿namespace Client
+{
+    public class Drawer
+    {
+        private bool _drawerOpen;
+
+        public bool DrawerOpen
+        {
+            get => _drawerOpen;
+            set => _drawerOpen = value;
+        }
+
+        public Drawer()
+        {
+            _drawerOpen = true;
+        }
+
+
+        public void DrawerToggle()
+        {
+            DrawerOpen = !DrawerOpen;
+        }
+    }
+}
