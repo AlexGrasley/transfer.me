0abfcc9b68d8384512243e19b1927d1c31df2678
Davis Henckel <51898571+DavisHenckel@users.noreply.github.com>
2022-02-27 17:03:19 -0800

    Created Client side code to issue query to Cosmos DB

---
+++ Client/Pages/GetFileTest.razor.cs
@@ -0,0 +1,18 @@
+﻿using Client.ViewModels;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using Client.Services;
+
+namespace Client.Pages
+{
+    public partial class GetFileTest : ComponentBase
+    {
+        public static FileIDInputVM FileObj { get; set; } = new FileIDInputVM();
+        public async void OnValidSubmit(EditContext Context)
+        {
+            HttpService wc = new HttpService();
+            await HttpService.PostAsync("api/FileUtil/download", FileObj.FileGUID);
+            StateHasChanged();
+        }
+    }
+}
