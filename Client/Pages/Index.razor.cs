010730ca428fba6a046ea08b28cb7d1c96486013
AlexGrasley <alexgrasley@gmail.com>
2022-03-04 12:02:53 -0800

    file uploads working, along with dialog box. Still need to figure out how to print the key

--- Client/Pages/Index.razor.cs
+++ Client/Pages/Index.razor.cs
@@ -13,35 +13,16 @@ namespace Client.Pages
         private bool isLoading = false;
         public static IList<EncFile> fileList = new List<EncFile>();
         private bool fileTooLarge = false;
-        private FileDescriptor? fileDescriptor;
-        [CascadingParameter] MudDialogInstance MudDialog { get; set; }
+        public static FileDescriptor? fileDescriptor;
 
-        void Submit() => MudDialog.Close(DialogResult.Ok(true));
-        void Cancel() => MudDialog.Cancel();
-
-        private void OnInputFileChanged(InputFileChangeEventArgs e)
+        private async void OnInputFileChanged(InputFileChangeEventArgs e)
         {
-            isLoading = true;
-            StateHasChanged();
-            fileDescriptor = FileUtilService.GetFiles(e);
-            isLoading = false;
-            StateHasChanged();
+            fileDescriptor = await FileUtilService.GetFiles(e);
             if (!FileUtilService.FileSizeIsOK())
             {
                 fileTooLarge = true;
                 StateHasChanged();
             }
-        }
-
-        private DialogService GetDialogService()
-        {
-            return DialogService;
-        }
-
-        private void OpenDialog(DialogService dialogService)
-        {
-            var options = new DialogOptions { CloseOnEscapeKey = true };
-            IDialogReference dialogReference = dialogService.Show<MudDialog>("Simple Dialog", options);
         }
 
         private void Clear()
