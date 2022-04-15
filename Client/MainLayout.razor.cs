20558d6cc460ade122e23eb760b271f5db449f84
Davis Henckel <51898571+DavisHenckel@users.noreply.github.com>
2022-04-15 07:23:29 -0700

    Instantiate themes class in MainLayout

--- Client/MainLayout.razor.cs
+++ Client/MainLayout.razor.cs
@@ -1,59 +1,14 @@
-﻿using MudBlazor;
-using MudBlazor.Services;
+﻿using Client.Themes;
 
 namespace Client
 {
-    public class Themes
+    public partial class MainLayout
     {
-        public bool IsDarkTheme { get; set; } = false;
-        public MudTheme CurrentTheme = new MudTheme();
+        public Theme ?MyTheme { get; set; }
 
-        public void ToggleDarkTheme()
+        protected override void OnInitialized()
         {
-            IsDarkTheme = !IsDarkTheme;
-            if (IsDarkTheme)
-            {
-                CurrentTheme = GenerateDarkTheme();
-            }
-            else
-            {
-                CurrentTheme = GenerateLightTheme();
-            }
-        }
-
-        private MudTheme GenerateDarkTheme()
-        {
-            var DTheme = new MudTheme
-            {
-                Palette = new Palette()
-                {
-                    Black = "#27272f",
-                    Background = "#32333d",
-                    BackgroundGrey = "#27272f",
-                    Primary = "#DC4405",
-                    Secondary = "#000000",
-                    Surface = "#373740",
-                    TextPrimary = "#FFFFFF",
-                    TextSecondary = "rgba(255,255,255, 0.50)"
-                }
-            };
-            return DTheme;
-        }
-
-        private MudTheme GenerateLightTheme()
-        {
-            var LTheme = new MudTheme
-            {
-                Palette = new Palette()
-                {
-                    Primary = "#DC4405",
-                    Secondary = "#FFFFFF",
-                    Surface = "#373740",
-                    TextPrimary = "#000000",
-                    TextSecondary = "rgba(255,255,255, 0.50)"
-                }
-            };
-            return LTheme;
+            MyTheme = new Theme();
         }
     }
 }
