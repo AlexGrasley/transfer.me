9dd4889823b454a71192b9e61df9792002db8046
Davis Henckel <51898571+DavisHenckel@users.noreply.github.com>
2022-04-15 07:09:38 -0700

    Add a Themes folder to store various themes.

---
+++ Client/Themes/Theme.cs
@@ -0,0 +1,58 @@
+﻿using MudBlazor;
+
+namespace Client.Themes
+{
+    public class Theme
+    {
+        public bool IsDarkTheme { get; set; } = false;
+        public MudTheme CurrentTheme = new MudTheme();
+
+        public void ToggleDarkTheme()
+        {
+            IsDarkTheme = !IsDarkTheme;
+            if (IsDarkTheme)
+            {
+                CurrentTheme = GenerateDarkTheme();
+            }
+            else
+            {
+                CurrentTheme = GenerateLightTheme();
+            }
+        }
+
+        private MudTheme GenerateDarkTheme()
+        {
+            var DTheme = new MudTheme
+            {
+                Palette = new Palette()
+                {
+                    Black = "#27272f",
+                    Background = "#32333d",
+                    BackgroundGrey = "#27272f",
+                    Primary = "#DC4405",
+                    Secondary = "#000000",
+                    Surface = "#373740",
+                    TextPrimary = "#FFFFFF",
+                    TextSecondary = "rgba(255,255,255, 0.50)"
+                }
+            };
+            return DTheme;
+        }
+
+        private MudTheme GenerateLightTheme()
+        {
+            var LTheme = new MudTheme
+            {
+                Palette = new Palette()
+                {
+                    Primary = "#DC4405",
+                    Secondary = "#FFFFFF",
+                    Surface = "#373740",
+                    TextPrimary = "#000000",
+                    TextSecondary = "rgba(255,255,255, 0.50)"
+                }
+            };
+            return LTheme;
+        }
+    }
+}
