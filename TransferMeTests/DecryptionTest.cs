494f75f5cc030abfddeb03a5701724cba5010896
Davis Henckel <51898571+DavisHenckel@users.noreply.github.com>
2022-05-15 08:14:36 -0700

    Successful decryption unit test.

---
+++ TransferMeTests/DecryptionTest.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using Client.Crypto;
+using System.Text;
+using Org.BouncyCastle.Crypto.Parameters;
+using System;
+
+namespace TransferMeTests
+{
+    public class DecryptionTest
+    {
+        [Test]
+        public void TestDecryption()
+        {
+            string TestString = "My Name is Davis and this is a unit test!";
+            byte[] key = AES.KeyGen();
+            ParametersWithIV keyParamsWithIV = AES.GenerateKeyWithIV(key);
+            byte[] bytes = Encoding.ASCII.GetBytes(TestString);
+            var EncString = AES.Encrypt(bytes, keyParamsWithIV);
+            Assert.AreNotEqual(EncString, bytes);
+            Assert.AreNotEqual(EncString.Length, bytes.Length);
+            string keystring = Convert.ToBase64String(key);
+            var DecBytes = AES.Decrypt(EncString, keystring);
+            Assert.AreEqual(DecBytes, bytes);
+
+        }
+    }
+}
